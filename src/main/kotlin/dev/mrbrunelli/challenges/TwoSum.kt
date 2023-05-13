4ae187621343ea3df11bc9b26a17aee002c9fa9c
mrbrunelli <matheus.brunelli@gmail.com>
2023-05-13 18:00:26 -0300

    add n2-1 solution

--- src/main/kotlin/dev/mrbrunelli/challenges/TwoSum.kt
+++ src/main/kotlin/dev/mrbrunelli/challenges/TwoSum.kt
@@ -1,11 +1,23 @@
 package dev.mrbrunelli.challenges
 
-//Input: nums = [2,7,11,15], target = 9
-//Output: [0,1]
-// 7 + 2 = 9
-
 class TwoSum {
-    fun play(list: IntArray, target: Int): IntArray? {
+    fun solution(list: IntArray, target: Int): IntArray {
+        val result = IntArray(2)
+        for (i in list.indices) {
+            var c = i + 1
+            while (c < list.size) {
+                if (list[i] + list[c] == target) {
+                    result[0] = i
+                    result[1] = c
+                    break
+                }
+                c++
+            }
+        }
+        return result
+    }
+
+    fun n2solution(list: IntArray, target: Int): IntArray? {
         for (item in list.withIndex()) {
             for (subItem in list.withIndex()) {
                 val sum = item.value + subItem.value
@@ -16,4 +28,6 @@ class TwoSum {
         }
         return null
     }
+
+
 }
--- src/test/kotlin/dev/mrbrunelli/challenges/TwoSumTest.kt
+++ src/test/kotlin/dev/mrbrunelli/challenges/TwoSumTest.kt
@@ -1,6 +1,7 @@
 package dev.mrbrunelli.challenges
 
 import org.junit.Test
+import kotlin.system.measureTimeMillis
 import kotlin.test.assertContentEquals
 
 class TwoSumTest {
@@ -8,16 +9,45 @@ class TwoSumTest {
 
     @Test
     fun `case 1`() {
-        assertContentEquals(intArrayOf(0, 1), sut.play(intArrayOf(2, 7, 11, 15), 9))
+        assertContentEquals(intArrayOf(0, 1), sut.n2solution(intArrayOf(2, 7, 11, 15), 9))
     }
 
     @Test
     fun `case 2`() {
-        assertContentEquals(intArrayOf(1, 2), sut.play(intArrayOf(3, 2, 4), 6))
+        assertContentEquals(intArrayOf(1, 2), sut.n2solution(intArrayOf(3, 2, 4), 6))
     }
 
     @Test
     fun `case 3`() {
-        assertContentEquals(intArrayOf(0, 1), sut.play(intArrayOf(3, 3), 6))
+        assertContentEquals(intArrayOf(0, 1), sut.n2solution(intArrayOf(3, 3), 6))
+    }
+
+    @Test
+    fun `case 4`() {
+        assertContentEquals(intArrayOf(0, 1), sut.solution(intArrayOf(2, 7, 11, 15), 9))
+    }
+
+    @Test
+    fun `case 5`() {
+        assertContentEquals(intArrayOf(1, 2), sut.solution(intArrayOf(3, 2, 4), 6))
+    }
+
+    @Test
+    fun `case 6`() {
+        assertContentEquals(intArrayOf(0, 1), sut.solution(intArrayOf(3, 3), 6))
+    }
+
+    @Test
+    fun `measure n2solution time`() {
+        val list = intArrayOf(2, 7, 11, 15)
+        val t1 = measureTimeMillis {
+            sut.solution(list, 9)
+        }
+        val t2 = measureTimeMillis {
+            sut.n2solution(list, 9)
+        }
+        println("solutionTime $t1 ms")
+        println("n2solutionTime $t2 ms")
+        assert(t1 < t2)
     }
 }
