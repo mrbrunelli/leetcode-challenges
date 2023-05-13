544a2dce00c23ce7e06b5c966506202ffc230a85
mrbrunelli <matheus.brunelli@gmail.com>
2023-05-13 13:25:18 -0300

    add more descriptive names

--- src/main/kotlin/dev/mrbrunelli/challenges/BinarySearch.kt
+++ src/main/kotlin/dev/mrbrunelli/challenges/BinarySearch.kt
@@ -4,16 +4,16 @@ class BinarySearch {
     // Binary search O(log(n)) is better than linear search O(N)
     // To search a number in 128 numbers using a O(N), we need to iterate a maximum of 128 times
     // To search a number in 128 numbers using a O(log(n)), we need to iterate a maximum of 7 times, which equals 2^7 = 128
-    fun play(n: Int, list: List<Int>): Int? {
-        var left = 0
-        var right = list.lastIndex
-        while (left <= right) {
-            val mid = (left + right) / 2
-            val bet = list[mid]
+    fun play(target: Int, list: List<Int>): Int? {
+        var leftPointer = 0
+        var rightPointer = list.lastIndex
+        while (leftPointer <= rightPointer) {
+            val middlePointer = (leftPointer + rightPointer) / 2
+            val attempt = list[middlePointer]
             when {
-                bet == n -> return mid
-                bet > n -> right = mid - 1
-                else -> left = mid + 1
+                attempt == target -> return middlePointer
+                attempt > target -> rightPointer = middlePointer - 1
+                else -> leftPointer = middlePointer + 1
             }
         }
         return null
