5cec70059bc94aea63f76389828f7ce30b8cf214
mrbrunelli <matheus.brunelli@gmail.com>
2023-05-13 14:23:05 -0300

    add performance's comparative test

--- src/main/kotlin/dev/mrbrunelli/challenges/BinarySearch.kt
+++ src/main/kotlin/dev/mrbrunelli/challenges/BinarySearch.kt
@@ -1,10 +1,11 @@
 package dev.mrbrunelli.challenges
 
 class BinarySearch {
+    // Big O notation
     // Binary search O(log(n)) is better than linear search O(N)
     // To search a number in 128 numbers using a O(N), we need to iterate a maximum of 128 times
     // To search a number in 128 numbers using a O(log(n)), we need to iterate a maximum of 7 times, which equals 2^7 = 128
-    fun play(target: Int, list: List<Int>): Int? {
+    fun binary(target: Int, list: List<Int>): Int? {
         var leftPointer = 0
         var rightPointer = list.lastIndex
         while (leftPointer <= rightPointer) {
@@ -14,6 +15,15 @@ class BinarySearch {
                 attempt == target -> return middlePointer
                 attempt > target -> rightPointer = middlePointer - 1
                 else -> leftPointer = middlePointer + 1
+            }
+        }
+        return null
+    }
+
+    fun linear(target: Int, list: List<Int>): Int? {
+        for ((index, item) in list.withIndex()) {
+            if (item == target) {
+                return index
             }
         }
         return null
--- src/test/kotlin/dev/mrbrunelli/challenges/BinarySearchTest.kt
+++ src/test/kotlin/dev/mrbrunelli/challenges/BinarySearchTest.kt
@@ -1,6 +1,7 @@
 package dev.mrbrunelli.challenges
 
 import org.junit.Test
+import kotlin.system.measureTimeMillis
 import kotlin.test.assertEquals
 
 class BinarySearchTest {
@@ -8,21 +9,45 @@ class BinarySearchTest {
 
     @Test
     fun `case 1`() {
-        assertEquals(32, sut.play(33, (1..100).toList()))
+        assertEquals(32, sut.binary(33, (1..100).toList()))
     }
 
     @Test
     fun `case 2`() {
-        assertEquals(1, sut.play(3, listOf(1, 3, 5, 7, 9)))
+        assertEquals(1, sut.binary(3, listOf(1, 3, 5, 7, 9)))
     }
 
     @Test
     fun `case 3`() {
-        assertEquals(4, sut.play(25, listOf(3, 8, 11, 17, 25, 41, 58, 100)))
+        assertEquals(4, sut.binary(25, listOf(3, 8, 11, 17, 25, 41, 58, 100)))
     }
 
     @Test
     fun `case 4`() {
-        assertEquals(null, sut.play(-1, (5..10).toList()))
+        assertEquals(null, sut.binary(-1, (5..10).toList()))
+    }
+
+    @Test
+    fun `case 5`() {
+        assertEquals(72, sut.linear(73, (1..100).toList()))
+    }
+
+    @Test
+    fun `case 6`() {
+        assertEquals(72, sut.binary(73, (1..100).toList()))
+    }
+
+    @Test
+    fun `case 7`() {
+        val list = (1..1_000_000).toList()
+        val binaryTime = measureTimeMillis {
+            sut.binary(600_000, list)
+        }
+        val linearTime = measureTimeMillis {
+            sut.linear(600_000, list)
+        }
+        println("binary time: $binaryTime ms")
+        println("linear time: $linearTime ms")
+        assert(binaryTime < linearTime)
     }
 }
