5d8acbada170b394de4fe6ef86ecc5c7b35455fd
mrbrunelli <matheus.brunelli@gmail.com>
2023-05-16 11:36:09 -0300

    add hash map solution

--- src/main/kotlin/dev/mrbrunelli/challenges/TwoSum.kt
+++ src/main/kotlin/dev/mrbrunelli/challenges/TwoSum.kt
@@ -1,7 +1,23 @@
 package dev.mrbrunelli.challenges
 
 class TwoSum {
-    fun solution(list: IntArray, target: Int): IntArray {
+    fun sol3(list: IntArray, target: Int): IntArray {
+        val m = hashMapOf<Int, Int>()
+        val r = IntArray(2)
+        for (i in list.indices) {
+            val diff = target - list[i]
+            if (m.contains(diff)) {
+                r[0] = m[diff] ?: 0
+                r[1] = i
+                return r
+            } else {
+                m[list[i]] = i
+            }
+        }
+        return r
+    }
+
+    fun sol2(list: IntArray, target: Int): IntArray {
         val result = IntArray(2)
         for (i in list.indices) {
             var c = i + 1
@@ -17,7 +33,7 @@ class TwoSum {
         return result
     }
 
-    fun n2solution(list: IntArray, target: Int): IntArray? {
+    fun sol1(list: IntArray, target: Int): IntArray? {
         for (item in list.withIndex()) {
             for (subItem in list.withIndex()) {
                 val sum = item.value + subItem.value
--- src/test/kotlin/dev/mrbrunelli/challenges/TwoSumTest.kt
+++ src/test/kotlin/dev/mrbrunelli/challenges/TwoSumTest.kt
@@ -1,6 +1,7 @@
 package dev.mrbrunelli.challenges
 
 import org.junit.Test
+import kotlin.system.measureNanoTime
 import kotlin.system.measureTimeMillis
 import kotlin.test.assertContentEquals
 
@@ -9,45 +10,65 @@ class TwoSumTest {
 
     @Test
     fun `case 1`() {
-        assertContentEquals(intArrayOf(0, 1), sut.n2solution(intArrayOf(2, 7, 11, 15), 9))
+        assertContentEquals(intArrayOf(0, 1), sut.sol1(intArrayOf(2, 7, 11, 15), 9))
     }
 
     @Test
     fun `case 2`() {
-        assertContentEquals(intArrayOf(1, 2), sut.n2solution(intArrayOf(3, 2, 4), 6))
+        assertContentEquals(intArrayOf(1, 2), sut.sol1(intArrayOf(3, 2, 4), 6))
     }
 
     @Test
     fun `case 3`() {
-        assertContentEquals(intArrayOf(0, 1), sut.n2solution(intArrayOf(3, 3), 6))
+        assertContentEquals(intArrayOf(0, 1), sut.sol1(intArrayOf(3, 3), 6))
     }
 
     @Test
     fun `case 4`() {
-        assertContentEquals(intArrayOf(0, 1), sut.solution(intArrayOf(2, 7, 11, 15), 9))
+        assertContentEquals(intArrayOf(0, 1), sut.sol2(intArrayOf(2, 7, 11, 15), 9))
     }
 
     @Test
     fun `case 5`() {
-        assertContentEquals(intArrayOf(1, 2), sut.solution(intArrayOf(3, 2, 4), 6))
+        assertContentEquals(intArrayOf(1, 2), sut.sol2(intArrayOf(3, 2, 4), 6))
     }
 
     @Test
     fun `case 6`() {
-        assertContentEquals(intArrayOf(0, 1), sut.solution(intArrayOf(3, 3), 6))
+        assertContentEquals(intArrayOf(0, 1), sut.sol2(intArrayOf(3, 3), 6))
     }
 
     @Test
-    fun `measure n2solution time`() {
+    fun `case 7`() {
+        assertContentEquals(intArrayOf(0, 1), sut.sol3(intArrayOf(2, 7, 11, 15), 9))
+    }
+
+    @Test
+    fun `case 8`() {
+        assertContentEquals(intArrayOf(1, 2), sut.sol3(intArrayOf(3, 2, 4), 6))
+    }
+
+    @Test
+    fun `case 9`() {
+        assertContentEquals(intArrayOf(0, 1), sut.sol3(intArrayOf(3, 3), 6))
+    }
+
+    @Test
+    fun `measure solutions time`() {
         val list = intArrayOf(2, 7, 11, 15)
-        val t1 = measureTimeMillis {
-            sut.solution(list, 9)
+        val t1 = measureNanoTime {
+            sut.sol2(list, 9)
         }
-        val t2 = measureTimeMillis {
-            sut.n2solution(list, 9)
+        val t2 = measureNanoTime {
+            sut.sol1(list, 9)
         }
-        println("solutionTime $t1 ms")
-        println("n2solutionTime $t2 ms")
+        val t3 = measureNanoTime {
+            sut.sol3(list, 9)
+        }
+        println("sol1 $t1 ns")
+        println("sol2 $t2 ns")
+        println("sol3 $t3 ns")
         assert(t1 < t2)
+        assert(t3 < t1)
     }
 }
